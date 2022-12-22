628d49be5d43b971ddf93dbf25f48810f5ca03d1
Manvi Gupta <manvigupta226@gmail.com>
2022-12-22 21:49:59 +0530

    connected database

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  host: "localhost",
+  host: "containers-us-west-140.railway.app",
   user: "postgres",
-  port: 5432,
-  password: "12345678",
-  database: "BankData",
+  port: 6037,
+  password: "uMP0s3chTxIS4OXHcm59",
+  database: "railway",
 });
 
 module.exports = client;
